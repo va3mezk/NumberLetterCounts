05726dfcf7331c348b78d152b5c78c4076e58504
va3mezk <va3mezk@gmail.com>
2021-11-14 08:03:34 -0600

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
-fun main(args: Array<String>) {
+fun main() {
     println(NumberLetters.countLetter(1000))
 }
--- src/test/kotlin/TestNumberLetters.kt
+++ src/test/kotlin/TestNumberLetters.kt
@@ -3,17 +3,17 @@ import kotlin.test.assertEquals
 
 class TestNumberLetters {
     @Test
-    fun `test #1`() = assertEquals(19, NumberLetters.countLetter(5))
+    fun `test one`() = assertEquals(19, NumberLetters.countLetter(5))
 
     @Test
-    fun `test #2`() = assertEquals(389, NumberLetters.countLetter(50))
+    fun `test fifty`() = assertEquals(389, NumberLetters.countLetter(50))
 
     @Test
-    fun `test #3`() = assertEquals(3068, NumberLetters.countLetter(222))
+    fun `test two hundred twenty-two`() = assertEquals(3068, NumberLetters.countLetter(222))
 
     @Test
-    fun `test #4`() = assertEquals(11331, NumberLetters.countLetter(645))
+    fun `test six hundred forty-five`() = assertEquals(11331, NumberLetters.countLetter(645))
 
     @Test
-    fun `test #5`() = assertEquals(18451, NumberLetters.countLetter(1000))
+    fun `test one thousand`() = assertEquals(18451, NumberLetters.countLetter(1000))
 }
